bedae663956e6172e3ec6150e54d62dbfb7c1453
DisJJ <jared.dyreson@gmail.com>
2016-07-05 18:12:29 -0700

    Update If Statement.sh

--- If Statement.sh
+++ If Statement.sh
@@ -1,10 +1,12 @@
 #messing around with blank input
+
 read -p "What SpringBoard theme will you be using and give the location of the file : " n1 
-if [[ ! $n1  =~ ^( "" ) ]]; then 
+
+if [[ -z "$n1" ]]; then 
 	echo "SpringBoard Theme name: NONE" >> Theme.txt
+
 #if not left blank
-elif [[ ! $n1 =~ ^( "$n1" ) ]]; then
-	find $n1 -type f -printf "SpringBoard Theme name: %f\n" >> Theme.txt
+
 else
-	echo error
+	find $n1 -type f -printf "SpringBoard Theme name: %f\n" >> Theme.txt
 fi
